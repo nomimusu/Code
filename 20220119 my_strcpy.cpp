b41047b7060796447db2044f22effc4ca4c78c03
nomimusu <97928993+nomimusu@users.noreply.github.com>
2022-02-17 18:41:46 +0800

    Update 20220119 my_strcpy.cpp

--- 20220119 my_strcpy.cpp
+++ 20220119 my_strcpy.cpp
@@ -1,31 +1,27 @@
 #include <stdio.h>
 #include <assert.h>
 
-char* my_strcpy(char* dest, const char* src)
+//const放在*左边，表示无法通过指针src来修改指针所保存的地址里面存储的值
+char* my_strcpy(char* dest, const char*src)
 {
-	//保证输入值合法，不是空指针
 	assert(dest != NULL);
 	assert(src != NULL);
 
+	//将dest的首元素地址传给另一个指针，保证有一个指针一直保存dest的首元素地址
 	char* ret = dest;
 
-	//const将src的存储空间的数据设置为常量
 	while (*dest++ = *src++)
 	{
 		;
 	}
 
-
 	return ret;
-
-
 }
 
 int main()
 {
-	char arr1[] = "################";
-	char arr2[] = "bit";
-	my_strcpy(arr1, arr2);
-	printf("%s\n", arr1);
+	char arr1[] = "#####################";
+	char arr2[] = "bits";
+	printf("%s\n",my_strcpy(arr1, arr2));
 	return 0;
 }
